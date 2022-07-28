fdf4e4953e0acf4e9951cfaed938a5b92d3017e3
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-28 09:38:09 +0300

    Create 05CombinationsWithoutRepetition.java

---
+++ 03 Recursion and Combinatorial Problems - Exercise/05CombinationsWithoutRepetition.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CombinationsWithoutRepetition {
+    private static Integer[] vector;
+    private static Integer[] temp;
+    private static int numberOfElementsToCombine;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        vector = new Integer[Integer.parseInt(reader.readLine())];
+        numberOfElementsToCombine = Integer.parseInt(reader.readLine());
+        temp = new Integer[numberOfElementsToCombine];
+
+        generateVector(0, 1);
+
+        System.out.println();
+    }
+
+    private static void generateVector(int index, int start) {
+
+        if (index >= numberOfElementsToCombine) {
+            System.out.println(Arrays.stream(temp).map(String::valueOf).collect(Collectors.joining(" ")));
+            return;
+        }
+
+        for (int i = start; i <= vector.length; i++) {
+            temp[index] = i;
+            generateVector(index + 1, i + 1);
+        }
+    }
+}
